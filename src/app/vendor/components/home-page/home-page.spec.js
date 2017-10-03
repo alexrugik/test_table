7d9f78e685a654654f7b6661079f7dd0c1dfa969
Ruzhinskiy <ARuzhinskiy@kiev.luxoft.com>
2017-10-03 12:24:05 +0300

    added some tests for components

---
+++ src/app/vendor/components/home-page/home-page.spec.js
@@ -0,0 +1,5 @@
+describe('test home-page component', () =>{
+   it('it should be error', () => {
+      expect(1).to.equal(1);
+   });
+});
---
+++ src/app/vendor/components/info-page/info-page.spec.js
@@ -0,0 +1,5 @@
+describe('test info-page componet', () => {
+   it('should 1 to be eqaual 1', () =>{
+      expect(1).to.equal(1);
+   });
+});
