ea61a27587f5dddc6bb990e9c335af00bb00d08c
Ruzhinskiy <ARuzhinskiy@kiev.luxoft.com>
2017-10-05 11:09:49 +0300

    added file loader

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,18 @@ const webpackConfig = {
                         attrs: [':data-src']
                     }
                 }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
